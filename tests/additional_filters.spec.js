174dde43f8502fe3283343f1fcd522f4c7608d3d
DESKTOP-RN8E3O3\user <dmytro.gursky@gmail.com>
2022-11-21 20:54:56 +0200

    add the second test to the additional_filters test

--- tests/additional_filters.spec.js
+++ tests/additional_filters.spec.js
@@ -10,8 +10,8 @@ test.beforeAll(async ({ browser }) => {
   page = await browser.newPage();
  
   const loginPage = new LoginPage(page)
-    await loginPage.goto();
-    await loginPage.validLogin();
+    await loginPage.goto(); //Перехід на сторінку логіну
+    await loginPage.validLogin(); //Логін користувача валідними данними
     
 });
 
@@ -20,41 +20,20 @@ test.beforeAll(async ({ browser }) => {
 //  */
 
 test('runs first', async () => {
-    //const adding_uses = new AddingUser(page)
-    const additional_filters = new AdditionalFilters(page)
-
-    //await page.pause()
+  const additional_filters = new AdditionalFilters(page)
     await additional_filters.chacBpx()
-
-    // await additional_filters.additionalFiltersBtn .click()
-    // await additional_filters.accessRightsBtn.click({ timeout: 5000 })
-    // await additional_filters.wait
-    //await page.waitForSelector(':nth-match(:text("Редактирование"), 1') //waitForTimeout(500)
-    //await page.waitForSelector('#PolarisOptionList1-1 label:has-text("Редактирование") div >> nth=2')
-    await additional_filters.edit1CheckBox.check()
-    expect(await page.locator('#PolarisOptionList1-1 label:has-text("Редактирование") div >> nth=2').isChecked()).toBeTruthy()
+    //перевірка чи проставлений чекбокс "Редактирование"
+    expect(await page.locator('#PolarisOptionList1-1 label:has-text("Редактирование") div >> nth=2').isChecked()).toBeTruthy() 
     await additional_filters.performedBtn.click()
-    expect(await page.getByAltText('Возможность: Редактирование сотрудников'));
-
-
-
-
-   
-    // await page.locator('text = Дополнительные фильтры').click()
-    // await page.locator('text = Права Доступа').click()
-    // await page.locator(':nth-match(:text("Редактирование"), 1)').check()
-    // expect(await page.locator('#PolarisOptionList1-1 label:has-text("Редактирование") div >> nth=2').isChecked()).toBeTruthy()
-    // await page.locator('text = Выполнено').click()
-    // expect(await page.getByAltText('Возможность: Редактирование сотрудников'));
-
+    expect(await page.getByAltText('Возможность: Редактирование сотрудников')); //первірка чи зявився фільтр під полем Редактирование сотрудников
 });
 
-// test.only('runs firsta', async () => {
-//   //const adding_uses = new AddingUser(page)
-//   const additional_filters = new AdditionalFilters(page)
-// await page.pause()
-// await additional_filters.chacBpx2()
-// await page.waitForTimeout(500)
+test('runs firsta', async () => {
+const additional_filters = new AdditionalFilters(page)
+    await additional_filters.chacBpx2()
+    //перевірка чи проставлений чекбокс "Чтение"
+    expect(await (additional_filters.readingCheckBox).isChecked()).toBeTruthy()
+    await additional_filters.performedBtn.click()
+    expect(await page.getByAltText('Возможность: Просмотр сотрудников')); //первірка чи зявився фільтр під полем пошуку співробітників
 
-
-// });
+});
--- tests/pages/additional_filters.page.js
+++ tests/pages/additional_filters.page.js
@@ -1,24 +1,30 @@
-const {expect, waitForTimeout, waitForSelector, isEditable} = require('@playwright/test');
+const {expect, waitForTimeout, waitForSelector, isEnabled, check} = require('@playwright/test');
 exports.AdditionalFilters = class AdditionalFilters {
     
 constructor(page) {
     this.page = page;
-    this.additionalFiltersBtn  = page.locator('text = Дополнительные фильтры')
-    this.accessRightsBtn = page.locator('text = Права Доступа') //Права Доступа 
-    this.waitForEdit1CheckBox = page.waitForSelector('#PolarisOptionList1-1 label:has-text("Редактирование") div >> nth=2')
-    this.edit1CheckBox = page.locator('#PolarisOptionList1-1 label:has-text("Редактирование") div >> nth=2') //ПРАВА ДОСТУПА СОТРУДНИКОВ - Редактирование
-    this.readingCheckBox = page.locator(':nth-match(:text("Чтение"), 1)') //ПРАВА ДОСТУПА СОТРУДНИКОВ - Чтение
+    this.additionalFiltersBtn  = page.locator('text = Дополнительные фильтры', { timeout: 1000 })
+    this.accessRightsBtn = page.locator('text = Права Доступа') //Права Доступа випадаючий список
+
+    // ПРАВА ДОСТУПА СОТРУДНИКОВ
+    this.edit1CheckBox = page.locator('#PolarisOptionList1-1 label:has-text("Редактирование") div >> nth=2') // Редактирование
+
+    this.readingCheckBox = page.locator(':nth-match(:text("Чтение"), 1)') // - Чтение
+
+
+    //РОЛИ
+
     this.performedBtn = page.locator('text = Выполнено')
-    this.wait = page.waitForTimeout(1000)
+    this.wait = page.waitForTimeout(2000)
     this.pause = page.pause()
 
     }
     async chacBpx() {
-    await this.additionalFiltersBtn.click({ timeout: 5000 });
-    await this.accessRightsBtn.click({ timeout: 10000 });
-    //await this.waitForSelector.edit1CheckBox
-    await this.edit1CheckBox.isEditable()
-
+    await this.additionalFiltersBtn.click();
+    await this.accessRightsBtn.click({ timeout: 1000 });
+    await this.edit1CheckBox.isEnabled()
+    await this.edit1CheckBox.check()
+    
     //await this.waitForEdit1CheckBox
 
     
@@ -34,16 +40,9 @@ constructor(page) {
 
 async chacBpx2() {
     await this.additionalFiltersBtn.click();
-    await this.accessRightsBtn.click();
-    await this.wait;
-    await this.readingCheckBox.click();
-    //перевірка чи проставлений чекбокс "Чтение"
-    expect(await page.locator('#PolarisOptionList1-1 label:has-text("Чтение") div >> nth=2').isChecked()).toBeTruthy()
-    //#PolarisOptionList1-1 >> text=Чтение
-    //#PolarisOptionList1-1 label:has-text("Чтение") div >> nth=2
-   await this.performedBtn.click()
-   //первірка чи зявився фільтр під полем пошуку співробітників
-   expect(await page.getByAltText('Возможность: Просмотр сотрудников'));
-
+    await this.accessRightsBtn.click({ timeout: 1000 });
+    await this.readingCheckBox.isEnabled();
+    await this.readingCheckBox.check()
+    //await this.readingCheckBox.click();
         }  
 }
